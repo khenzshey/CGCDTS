05f52488f065ec1594790775ce69748a2a56c039
Jabatskie <131598392+Jabatskie@users.noreply.github.com>
2023-05-22 11:59:39 +0800

    Update sidebar_header.php

--- includes/sidebar_header.php
+++ includes/sidebar_header.php
@@ -1,27 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php require_once '../config/connection.php'; ?>
+
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
 	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+	<meta name="keywords"
+		content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
 	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
-    <link rel="stylesheet" href="../css/all.min.css">
-    <link rel="stylesheet" href="../css/cgcdts.css?v.1">
+	<link rel="stylesheet" href="../css/all.min.css">
+	<link rel="stylesheet" href="../css/cgcdts.css?v.1">
 
 	<title>AdminKit Demo - Bootstrap 5 Admin Template</title>
-    <script src="../js/all.min.js"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="../js/all.min.js"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<link href="../css/app.css" rel="stylesheet">
-	
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
@@ -29,8 +32,8 @@
 		<nav id="sidebar" class="sidebar js-sidebar">
 			<div class="sidebar-content js-simplebar">
 				<a class="sidebar-brand" href="index.html">
-          <span class="align-middle">CGCDTS</span>
-        </a>
+					<span class="align-middle">CGCDTS</span>
+				</a>
 				<ul class="sidebar-nav">
 					<li class="sidebar-header">
 						Pages
@@ -38,32 +41,32 @@
 
 					<li class="sidebar-item">
 						<a class="sidebar-link" href="index.php">
-                        <i class="far fa-chart-pie-alt"></i><span class="align-middle">Dashboard</span>
-            </a>
+							<i class="far fa-chart-pie-alt"></i><span class="align-middle">Dashboard</span>
+						</a>
 					</li>
 
 					<li class="sidebar-item">
 						<a class="sidebar-link" href="pages-profile.html">
-                        <i class="far fa-users"></i><span class="align-middle">User Management</span>
-            </a>
+							<i class="far fa-users"></i><span class="align-middle">User Management</span>
+						</a>
 					</li>
 
-                    <li class="sidebar-item">
+					<li class="sidebar-item">
 						<a class="sidebar-link" href="office_management.php">
-                        <i class="far fa-users"></i><span class="align-middle">Office Management</span>
-            </a>
+							<i class="far fa-building"></i><span class="align-middle">Office Management</span>
+						</a>
 					</li>
 
 					<li class="sidebar-item">
 						<a class="sidebar-link" href="pages-sign-in.html">
-                        <i class="fal fa-file-alt"></i> <span class="align-middle">Document List</span>
-            </a>
+							<i class="fal fa-file-alt"></i> <span class="align-middle">Document List</span>
+						</a>
 					</li>
 
-                    <li class="sidebar-item">
+					<li class="sidebar-item">
 						<a class="sidebar-link" href="pages-sign-in.html">
-                        <i class="fad fa-analytics"></i> <span class="align-middle">Reports</span>
-            </a>
+							<i class="fad fa-analytics"></i> <span class="align-middle">Reports</span>
+						</a>
 					</li>
 				</ul>
 
@@ -73,8 +76,8 @@
 		<div class="main">
 			<nav class="navbar navbar-expand navbar-light navbar-bg">
 				<a class="sidebar-toggle js-sidebar-toggle">
-          <i class="hamburger align-self-center"></i>
-        </a>
+					<i class="hamburger align-self-center"></i>
+				</a>
 
 				<div class="navbar-collapse collapse">
 					<ul class="navbar-nav navbar-align">
@@ -85,7 +88,8 @@
 									<span class="indicator">4</span>
 								</div>
 							</a>
-							<div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0" aria-labelledby="alertsDropdown">
+							<div class="dropdown-menu dropdown-menu-lg dropdown-menu-end py-0"
+								aria-labelledby="alertsDropdown">
 								<div class="dropdown-menu-header">
 									4 New Notifications
 								</div>
@@ -97,7 +101,8 @@
 											</div>
 											<div class="col-10">
 												<div class="text-dark">Update completed</div>
-												<div class="text-muted small mt-1">Restart server 12 to complete the update.</div>
+												<div class="text-muted small mt-1">Restart server 12 to complete the
+													update.</div>
 												<div class="text-muted small mt-1">30m ago</div>
 											</div>
 										</div>
@@ -109,7 +114,8 @@
 											</div>
 											<div class="col-10">
 												<div class="text-dark">Lorem ipsum</div>
-												<div class="text-muted small mt-1">Aliquam ex eros, imperdiet vulputate hendrerit et.</div>
+												<div class="text-muted small mt-1">Aliquam ex eros, imperdiet vulputate
+													hendrerit et.</div>
 												<div class="text-muted small mt-1">2h ago</div>
 											</div>
 										</div>
@@ -132,7 +138,8 @@
 											</div>
 											<div class="col-10">
 												<div class="text-dark">New connection</div>
-												<div class="text-muted small mt-1">Christina accepted your request.</div>
+												<div class="text-muted small mt-1">Christina accepted your request.
+												</div>
 												<div class="text-muted small mt-1">14h ago</div>
 											</div>
 										</div>
@@ -143,23 +150,24 @@
 								</div>
 							</div>
 						</li>
-							<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                <img src="../img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
-              </a>
-							<div class="dropdown-menu dropdown-menu-end">
-								<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>
-								<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="help-circle"></i> Help Center</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#">Log out</a>
-							</div>
+						<a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
+							<img src="../img/avatars/avatar.jpg" class="avatar img-fluid rounded me-1"
+								alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
+						</a>
+						<div class="dropdown-menu dropdown-menu-end">
+							<a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1"
+									data-feather="user"></i> Profile</a>
+							<a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i>
+								Analytics</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="index.html"><i class="align-middle me-1"
+									data-feather="settings"></i> Settings & Privacy</a>
+							<a class="dropdown-item" href="#"><i class="align-middle me-1"
+									data-feather="help-circle"></i> Help Center</a>
+							<div class="dropdown-divider"></div>
+							<a class="dropdown-item" href="#">Log out</a>
+						</div>
 						</li>
 					</ul>
 				</div>
 			</nav>
-
-			
-
-			
