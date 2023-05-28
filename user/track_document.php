071d917240dfc583e76858f7db482a09f28b932c
Zreon <j.jabat@bcrvtvi.edu.ph>
2023-05-28 16:12:42 +0800

    Tracking edit

---
+++ user/track_document.php
@@ -0,0 +1,40 @@
+<?php require_once '../includes/sidebar_header.php' ?> 
+
+<main class="content">
+    <div class="progressContainer">
+
+<div style="margin-left:0%">
+<ul class="StepProgress">
+<div class="ofc-card d-flex justify-content-start flex p-2 gap-3">
+<div class="ofc-item w-50">
+            <div class="card-body">
+ <center><h2>City Government of Calapan Document Tracking System</h2></center>
+  <div class="StepProgress-item is-done"><strong>Human Resource</strong>
+  <p>May 29, 2023 : 10:44 AM <br> Completed... </p>
+  </div>
+  <div class="StepProgress-item is-done"><strong>Accounting Office</strong>
+  <p>May 29, 2023 : 10:49 AM <br> Completed... </p>
+  </div>
+  <div class="StepProgress-item is-done"><strong>Ano ga are?</strong>
+  <p>May 29, 2023 : 10:49 AM <br> Completed... </p>
+  </div>
+  <div class="StepProgress-item is-done"><strong>Kahit ano</strong>
+  <p>May 29, 2023 : 10:49 AM <br> Pending... </p>
+    </div>
+  <!-- <div class="StepProgress-item current"><strong>Bahala na</strong>
+  <p>May 29, 2023 : 10:49 AM</p>
+</div> -->
+</div>
+<div>
+      <div class="ofc-item w-50">
+      <input  type="text" placeholder="Enter your document ID" />
+       <button type="track">
+          Track
+       </button>
+
+</ul>
+</div>
+</div>
+</main>
+<div></div>
+<?php require_once '../includes/sidebar_footer.php' ?> 
